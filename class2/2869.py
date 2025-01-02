bddd833b9ee23dc03e0b1fcfcd36ddb5157ed23e
unknown <xowls0319@pusan.ac.kr>
2025-01-02 18:48:51 +0900

    25/01/02

--- class2/2869.py
+++ class2/2869.py
@@ -1,21 +1,20 @@
 #input을 빠르게 받기 위한 sys 라이브러리 사용
 import sys
+import math
 input = lambda: sys.stdin.readline().rstrip()
 
 if __name__ == "__main__":
     """
     [설계]
-    높이가 V미터인 나무 막대를 오른다.
-    낮에는 A미터 올라가고, 밤에는 B미터 미끄러진다.
-    정상에 올라간 후에는 미끄러지지 않고, 나무 막대를 모두 올라가려면
-    몇일이 걸리는지?
+    1. while문 사용 -> 시간초과
+    2. 시간초과를 줄이려면 -> V <= A + N*(A-B)이 성립하면 됨
+    이러면 총 일은 1 + N으로 해결
+    올라가야 할 높이 -> V - B 
     """
 
     A, B, V = map(int, input().split())
-    current_height = A
-    up_days = 1
-    while(current_height >= V):
-        current_height -= B
-        current_height += A
-        up_days += 1
-    print(up_days)
+    N = 0
+    days_up_height = V - B
+    oneday_up_height = A - B
+    up_days = days_up_height / oneday_up_height
+    print(math.ceil(up_days))
