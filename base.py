38c75db219bd9d0352913bc0f976aeb7747686eb
mireutal <mireutale@gmail.com>
2025-03-06 09:24:42 +0900

    0x03 문제 5개 품

--- base.py
+++ base.py
@@ -2,16 +2,17 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-if __name__ == "__main__":
-    """
-    [설계]
-    
-    
-    """
+"""
+[설계]
 
 
 """
-sys.stdin.readline()을 사용해서 input보다 더 빠르게 값을 입력받음
-rstrip()을 사용해서 마지막 개행문자 '\n'을 입력받지 않도록 설정
-lambda함수를 사용해서 input을 sys.stdin.readline().rstrip()으로 대체
+    
+if __name__ == "__main__":
+    
+
+"""
+    sys.stdin.readline()을 사용해서 input보다 더 빠르게 값을 입력받음
+    rstrip()을 사용해서 마지막 개행문자 '\n'을 입력받지 않도록 설정
+    lambda함수를 사용해서 input을 sys.stdin.readline().rstrip()으로 대체
 """
