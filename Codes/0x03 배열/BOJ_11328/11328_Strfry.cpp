a6fd7ab4db34266f05fdf45cddf6c645109b48ac
gykim_kr <k_bright_st@pusan.ac.kr>
2025-03-06 10:14:08 +0900

    *feat : 11328_strfry 풀이방식 추가

--- Codes/0x03 배열/BOJ_11328/11328_Strfry.cpp
+++ Codes/0x03 배열/BOJ_11328/11328_Strfry.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 /**
  * @brief  무작위 재배열이 가능한지를 확인하는 코드입니다. map을 활용하면 더욱 효율적일 것이나, 배열로 푸는 것이 목적으로 보입니다.
  * @return
  */
-int main() {
+void func1() {
     int eng_arr[26];
     int num = 0;
 
@@ -35,6 +35,38 @@ int main() {
         cout << "Possible" << "\n";
         cont:;
     }
+}
 
+/**
+ * @brief 이런 방법도 있음. 벡터 컨테이너로 비교하는 방식.
+ */
+void func2(){
+    int num = 0;
+    cin >> num;
+    while(num--){
+        vector<int> vecFirst(26, 0);
+        vector<int> vecSecond(26, 0);
+        string FirstString, SecondString;
+        cin >> FirstString >> SecondString;
+
+        for (auto word : FirstString)
+        {
+            ++vecFirst[word - 'a'];
+        }
+        for (auto word : SecondString)
+        {
+            ++vecSecond[word - 'a'];
+        }
+
+        if(vecFirst == vecSecond)
+            cout << "Possible\n";
+        else
+            cout << "Impossible\n";
+    }
+}
+
+int main(){
+    func1();
+//    func2();
     return 0;
 }
