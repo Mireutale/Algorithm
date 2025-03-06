51b00d677f8a38178930f6ab1508fd0bdb9073ab
mireutal <mireutale@gmail.com>
2025-03-06 15:16:01 +0900

    0x04배열 요세푸스 문제 해결

---
+++ Codes/0x04 연결리스트/BOJ_1158/1158_요세푸스문제.py
@@ -0,0 +1,24 @@
+#input을 빠르게 받기 위한 sys 라이브러리 사용
+import sys
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
+
+"""
+[설계]
+
+collections 라이브러리의 deque 사용
+deque는 double-ended queue의 약자로, 양방향 삽입 삭제가 가능한 데이터를 의미
+내부적으로는 링크드 리스트의 형태를 사용하고 있음.
+
+1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 순서대로 K번째 사람을 제거
+원에서 사람들이 제거되는 순서를 출력
+"""
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    link_list = deque(i + 1 for i in range(N)) 
+    Josephus = []
+    while(len(link_list) > 0):
+        link_list.rotate(-K)
+        Josephus.append(link_list.pop())
+    print("<" + ", ".join(str(num) for num in Josephus) + ">")
