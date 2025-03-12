faf0e9938088819025992569395c7372917ca751
mireutal <mireutale@gmail.com>
2025-03-12 14:46:26 +0900

    히스토그램 사각형 푸는중 _ mireutale

---
+++ Codes/0x05 스택/BOJ_6549/6549_히스토그램에서가장큰직사각형.py
@@ -0,0 +1,13 @@
+#input을 빠르게 받기 위한 sys 라이브러리 사용
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+"""
+[설계]
+히스토그램 : 직사각형 여러 개가 아래쪽으로 정렬되어 있는 도형
+직사각형은 같은 너비를 가지지만, 높이는 다름
+히스토그램에서 가장 넓이가 큰 직사각형을 구하는 프로그램
+"""
+
+if __name__ == "__main__":
+    pass
