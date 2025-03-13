1ece4c47e16f943dbc7ac573ff0b0b0ec6c8edd3
mireutal <mireutale@gmail.com>
2025-03-13 17:29:46 +0900

    0x05 큐 문제풀이_mireutale

---
+++ Codes/0x06 큐/BOJ_2164/2164_카드2.py
@@ -0,0 +1,27 @@
+#input을 빠르게 받기 위한 sys 라이브러리 사용
+import sys
+from collections import deque
+input = lambda: sys.stdin.readline().rstrip()
+
+"""
+[설계]
+
+N장의 카드가 순서대로 놓여있다.
+1번이 가장 위, N번 카드가 제일 아래인 상태
+
+카드가 한장 남을 때 까지, 제일 위에있는 카드를 바닥에 버리고
+제일 위에있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+
+마지막으로 남게되는 카드를 구하기.
+
+큐는 FIFO이므로, pop을 한 데이터가 제일 위에있는 카드
+append를 한 데이터가 제일 아래에 있는 카드 밑으로 이동하게 된다.
+"""
+
+if __name__ == "__main__":
+    n = int(input())
+    queue = deque(i+1 for i in range(n))
+    while len(queue) != 1:
+        queue.popleft()
+        queue.append(queue.popleft())
+    print(queue.pop())
