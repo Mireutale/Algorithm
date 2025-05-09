43fb3c4c683c8978575f913054972ea9b7839fec
mireutale <mireutale@gmail.com>
2025-05-09 18:15:03 +0900

    컨테이너벨트위의로봇 문제풀이_mireutale[25/05/09]

--- Codes/something/BOJ_20055/20055_컨테이너벨트위의로봇.py
+++ Codes/something/BOJ_20055/20055_컨테이너벨트위의로봇.py
@@ -11,30 +11,31 @@ def rotate_belt(belt_durability, robot_position):
     # 벨트 및 로봇 회전
     belt_durability.rotate(1)
     robot_position.rotate(1)
+    robot_position[belt_range-1] = 0
 
 def move_robots(belt_durability, robot_position, belt_range, belt_durability_count_zero):
     # 로봇 걷기
-    for i in range(len(robot_position)):
+    for i in range(len(robot_position) -2, -1, -1):
+        # 로봇이 벨트 위에 있는 경우
         if robot_position[i] == 1:
-            if i + 1 == len(robot_position):
+            if robot_position[i+1] == 0 and belt_durability[i+1] > 0:
                 robot_position[i] = 0
-            else:
-                if belt_durability[i+1] != 0:
-                    robot_position[i], robot_position[i+1] = robot_position[i+1], robot_position[i]
-                    belt_durability[i+1] -= 1
-                    if belt_durability[i+1] == 0:
-                        belt_durability_count_zero += 1
+                robot_position[i+1] = 1
+                belt_durability[i+1] -= 1
+                if belt_durability[i+1] == 0:
+                    belt_durability_count_zero += 1
+    # 로봇이 내리는 위치에 있는 경우
+    robot_position[belt_range-1] = 0
     return belt_durability_count_zero
 
 
 def load_robot(belt_durability, robot_position, belt_durability_count_zero):
     # 로봇 올리기
-    if belt_durability[0] > 0:
-        if robot_position[0] == 0:
-            robot_position[0] = 1
-            belt_durability[0] -= 1
-            if belt_durability[0] == 0:
-                belt_durability_count_zero += 1
+    if belt_durability[0] > 0 and robot_position[0] == 0:
+        robot_position[0] = 1
+        belt_durability[0] -= 1
+        if belt_durability[0] == 0:
+            belt_durability_count_zero += 1
     return belt_durability_count_zero
 
 if __name__ == "__main__":
@@ -47,22 +48,18 @@ if __name__ == "__main__":
         answer += 1
         # 벨트 회전
         rotate_belt(belt_durability, robot_position)
-        print("after rotate", belt_durability)
+        # print("after rotate", belt_durability, robot_position)
 
         # 로봇 걷기
-        print("robot position", robot_position)
         belt_durability_count_zero = move_robots(
             belt_durability, robot_position, belt_range, belt_durability_count_zero
         )
-        print("after walk", belt_durability)
+        # print("after walk", belt_durability, robot_position)
         if zero_durability_count <= belt_durability_count_zero:
             print(answer)
             exit()
 
         # 로봇 올리기
         belt_durability_count_zero = load_robot(belt_durability, robot_position, belt_durability_count_zero)
-        print("after load", belt_durability)
+        #print("after load", belt_durability, robot_position, answer)
     print(answer)
-
-# commit msg
-# --- 문제풀이_mireutale[25/ / ]
