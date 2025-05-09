a189e024a4276a741aca45291b766b2052baeadb
Mireutale <mireutale@gmail.com>
2025-05-09 15:22:10 +0900

    톱니바퀴 문제풀이_mireutale[25/05/09]

---
+++ Codes/something/BOJ_14891/14891_톱니바퀴.py
@@ -0,0 +1,14 @@
+"""
+* Codes\something\BOJ_14891\14891_톱니바퀴.py
+* Author : mireutale
+"""
+
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    pass
+
+# commit msg
+# --- 문제풀이_mireutale[25/ / ]
