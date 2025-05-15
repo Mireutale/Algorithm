43b9714ebf7e2fb9c8070854b69d45d508fca5f7
mireutale <mireutale@gmail.com>
2025-05-16 01:37:37 +0900

    벽부수고이동하기3 문제풀이_mireutale[25/05/15]

--- Codes/0x09 BFS/BOJ_16933/16033_벽부수고이동하기3.py
+++ Codes/0x09 BFS/BOJ_16933/16033_벽부수고이동하기3.py
@@ -1,5 +1,5 @@
 """
-* Codes\0x09 BFS\BOJ_14442\14442_벽부수고이동하기2.py
+* Codes\0x09 BFS\BOJ_16933\16033_벽부수고이동하기3.py
 * Author : mireutale
 """
 
@@ -13,28 +13,35 @@ if __name__ == "__main__":
     
     visited = [[[0] * m for _ in range(n)] for _ in range(crash+1)]
     direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
-    queue = deque()
-
     visited[0][0][0] = 1
-    queue.append([0, 0, 0])
+    queue = deque()
+    queue.append([0, 0, 0, 1]) # y, x, 벽을 깬 횟수, 거리
 
     while queue:
         # print(queue)
-        y, x, crash_cnt = queue.popleft()
+        y, x, crash_cnt, distance = queue.popleft()
         if y == n-1 and x == m-1:
-            print(visited[crash_cnt][y][x])
+            print(distance)
             exit()
+        day_or_night = distance % 2 # 1이면 낮, 0이면 밤
         for dy, dx in direction:
             ny, nx = y + dy, x + dx
             # 현재 can_crash가 visited에 저장된 값보다 작다면, 아래 코드 skip!
             if 0 <= ny < n and 0 <= nx < m:
+                # 빈 공간
                 if board[ny][nx] == 0 and not visited[crash_cnt][ny][nx]:
                     visited[crash_cnt][ny][nx] = visited[crash_cnt][y][x] + 1
-                    queue.append([ny, nx, crash_cnt])
-                else:
-                    if crash_cnt < crash and not visited[crash_cnt+1][ny][nx]:
+                    queue.append([ny, nx, crash_cnt, distance + 1])
+                # 벽
+                if board[ny][nx] == 1 and crash_cnt < crash and not visited[crash_cnt+1][ny][nx]:
+                    if day_or_night: # 아침
                         visited[crash_cnt+1][ny][nx] = visited[crash_cnt][y][x] + 1
-                        queue.append([ny, nx, crash_cnt + 1])
+                        queue.append([ny, nx, crash_cnt + 1, distance + 1])
+                    else: # 밤
+                        queue.append([y, x, crash_cnt, distance + 1])
     
     print(-1)
+
+
+# commit msg
+# --- 문제풀이_mireutale[25/ / ]
