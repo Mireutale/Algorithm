28856829d6fe83fe647e20f39b0b678cc7136f1b
Mireutale <mireutale@gmail.com>
2025-05-19 21:35:45 +0900

    계단 오르기 문제 풀이중

---
+++ Codes/0x10 다이나믹프로그래밍/BOJ_2579/2579_계단오르기.py
@@ -0,0 +1,14 @@
+"""
+* Codes\0x10 다이나믹프로그래밍\BOJ_2579\2579_계단오르기.py
+* Author : mireutale
+"""
+
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    pass
+
+# commit msg
+# --- 문제풀이_mireutale[25/ / ]
