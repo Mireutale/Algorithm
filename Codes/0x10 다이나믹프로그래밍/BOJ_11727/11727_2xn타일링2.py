979fcb77aa5c574d569a1772ebce2eeab315cb40
mireutale <mireutale@gmail.com>
2025-05-22 13:09:34 +0900

    11727 문제 풀이중..

---
+++ Codes/0x10 다이나믹프로그래밍/BOJ_11727/11727_2xn타일링2.py
@@ -0,0 +1,13 @@
+"""
+* Codes/0x10 다이나믹프로그래밍/BOJ_11727/11727_2xn타일링2.py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    pass
+
+# commit msg
+# --- 문제풀이_mireutale[25/ / ]
