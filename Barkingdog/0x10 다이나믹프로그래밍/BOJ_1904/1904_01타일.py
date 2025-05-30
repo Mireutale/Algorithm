8add74abcd331826e14940a625f75e9d86a6a82d
Mireutale <mireutale@gmail.com>
2025-05-30 14:26:45 +0900

    Solved - 01타일 문제풀이 Date : [25/05/30]

---
+++ Barkingdog/0x10 다이나믹프로그래밍/BOJ_1904/1904_01타일.py
@@ -0,0 +1,19 @@
+"""
+* CodingTest\Barkingdog\0x10 다이나믹프로그래밍\BOJ_1904\1904_01타일.py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    n = int(input())
+    dp = [0] * n
+    for i in range(n):
+        if i == 0:
+            dp[i] = 1
+        elif i == 1:
+            dp[i] = 2
+        else:
+            dp[i] = (dp[i-2] + dp[i-1]) % 15746
+    print(dp[-1])
