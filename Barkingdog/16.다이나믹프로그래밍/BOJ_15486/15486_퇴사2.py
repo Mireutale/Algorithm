2d73baf6855c5ce9b7181df01f92319474583309
mireutale <mireutale@gmail.com>
2025-06-17 20:31:25 +0900

    Solved / Working - 15486 퇴사2 문제풀이 Date : [25/06/17]

---
+++ Barkingdog/16.다이나믹프로그래밍/BOJ_15486/15486_퇴사2.py
@@ -0,0 +1,20 @@
+"""
+* CodingTest\Barkingdog\16.다이나믹프로그래밍\BOJ_15486\15486_퇴사2.py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    n = int(input())
+    work = [list(map(int, input().split())) for _ in range(n)]
+    dp = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):
+        time, pay = work[i]
+        finish_day = i + time
+        if finish_day <= n:
+            dp[i] = max(dp[i + 1], dp[finish_day] + pay)
+        else:
+            dp[i] = dp[i + 1]
+    print(dp[0])
