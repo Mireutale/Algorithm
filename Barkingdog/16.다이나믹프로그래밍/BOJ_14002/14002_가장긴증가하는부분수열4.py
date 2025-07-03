69daf44aa087c1294236d297600ea494bf1587e4
mireutale <mireutale@gmail.com>
2025-07-04 00:26:14 +0900

    Working: 14002 가장 긴 증가하는 부분수열 4 문제 푸는 중

---
+++ Barkingdog/16.다이나믹프로그래밍/BOJ_14002/14002_가장긴증가하는부분수열4.py
@@ -0,0 +1,25 @@
+"""
+* Codes/0x09 BFS/BOJ_2206/2206_벽부수고이동하기.py
+* Author : mireutale
+
+* input을 빠르게 받기 위한 sys 라이브러리 사용
+* sys.stdin.readline()을 사용해서 input보다 더 빠르게 값을 입력받음
+* rstrip()을 사용해서 마지막 개행문자 '\n'을 입력받지 않도록 설정
+* lambda함수를 사용해서 input을 sys.stdin.readline().rstrip()으로 대체
+"""
+
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    pass
+
+"""
+commit msg
+Solved / Working
+
+- Body
+
+Date : [25/06/01] 
+"""
