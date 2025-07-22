f25fcf82d59b00e46289e0325ec0bdd4e59f8fb9
mireutale <mireutale@gmail.com>
2025-07-22 12:27:26 +0900

    Barkingdog 내부 폴더명 변경

---
+++ Barkingdog/11.재귀/BOJ_1074/1074_Z.py
@@ -0,0 +1,10 @@
+"""
+* Barkingdog/11(0x0B).재귀/BOJ_1074/1074_Z.py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    pass
---
+++ Barkingdog/11.재귀/BOJ_1074/README.md
@@ -0,0 +1,11 @@
+# 백준 1074번 - Z
+
+[문제 보러 가기](https://www.acmicpc.net/problem/1074)
+
+## 요약
+
+## 핵심 규칙
+
+## 예제 입출력
+
+입력 및 출력 예시는 [문제 링크](https://www.acmicpc.net/problem/1074) 참고.
