58cbde7eb2f948c19d672f6c5bf306e5ccf66186
mireutale <mireutale@gmail.com>
2025-07-23 17:51:46 +0900

    Solved[#14956]: 철학자의 길 문제풀이

--- Barkingdog/11.재귀/BOJ_14956/14956_Philosopher's Walk.py
+++ Barkingdog/11.재귀/BOJ_14956/14956_Philosopher's Walk.py
@@ -1,10 +1,34 @@
 """
-* CodingTest/Barkingdog/11.재귀/BOJ_14956/14956_Philosopher's Walk.py
+* CodingTest/Barkingdog/11.재귀/BOJ_14956/14956_Philosopher's m.py
 * Author : mireutale
 """
 
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+def Philosophers_walk(n, m):
+    # Base case: n == 2일 때 직접 좌표 반환
+    if n == 2:
+        move = [(1, 1), (1, 2), (2, 2), (2, 1)]
+        return move[m - 1]
+
+    side = n // 2
+    area = side**2
+
+    if m <= area:
+        x, y = Philosophers_walk(side, m)
+        return (y, x)
+    elif m <= 2 * area:
+        x, y = Philosophers_walk(side, m - area)
+        return (x, y + side)
+    elif m <= 3 * area:
+        x, y = Philosophers_walk(side, m - 2 * area)
+        return (x + side, y + side)
+    else:
+        x, y = Philosophers_walk(side, m - 3 * area)
+        return (2 * side - y + 1, side - x + 1)
+
 if __name__ == "__main__":
-    pass
+    n, m = map(int, input().split())
+    x, y = Philosophers_walk(n, m)
+    print(x, y)
