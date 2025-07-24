ec7f6e1610befe75da3342d6355410c0d89c1a5d
mireutale <mireutale@gmail.com>
2025-07-24 12:25:33 +0900

    Solved[#15649]: N과 M (1) 문제풀이

---
+++ Barkingdog/12.백트래킹/BOJ_15649/15649_N과 M(1).py
@@ -0,0 +1,27 @@
+"""
+* CodingTest/Barkingdog/12.백트래킹/15649_N과 M(1).py
+* Author : mireutale
+"""
+
+import sys
+# import itertools import permutaions
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    ans = []
+
+    def back_tracking():
+        if len(ans) == m:
+            print(*ans)
+            return
+        
+        for i in range(n):
+            if (i+1) not in ans:
+                ans.append(i+1)
+                back_tracking()
+                ans.pop()
+
+    back_tracking()
+    # ans = permutation(range(1, n+1), m)
+    # print(*ans)
