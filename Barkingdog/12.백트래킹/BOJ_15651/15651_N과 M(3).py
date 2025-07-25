7472c8279946127e5af5c73158165b4f0b05351b
mireutale <mireutale@gmail.com>
2025-07-25 10:10:56 +0900

    Solved[#15651]: N과 M(3) 문제풀이

---
+++ Barkingdog/12.백트래킹/BOJ_15651/15651_N과 M(3).py
@@ -0,0 +1,23 @@
+"""
+* CodingTest/Barkingdog/12.백트래킹/15651_N과 M(3).py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    ans = []
+
+    def back_tracking():
+        if len(ans) == m:
+            print(*ans)
+            return
+        
+        for i in range(1, n + 1):
+            ans.append(i)
+            back_tracking()
+            ans.pop()
+
+    back_tracking()
