7b936b9ca786dae9498f93613f14e309a1f5f445
mireutale <mireutale@gmail.com>
2025-08-01 17:28:05 +0900

    Solved[#21598]: SciComLove 문제풀이

---
+++ starcoin/21598_SciComLove.py
@@ -0,0 +1,12 @@
+"""
+* git/CodingTest/Starcoin/21598_SciComLove.py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    n = int(input())
+    for _ in range(n):
+        print("SciComLove")
