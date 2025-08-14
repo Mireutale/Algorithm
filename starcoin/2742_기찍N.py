5d01a331304391adb4ce26331a6aa49ac6f2ef46
mireutale <mireutale@gmail.com>
2025-08-14 17:46:41 +0900

    Solved[#2742]: 기찍 N 문제풀이

---
+++ starcoin/2742_기찍N.py
@@ -0,0 +1,12 @@
+"""
+* CodingTest/Starcoin/2742_기찍N.py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    n = int(input())
+    for i in range(5, 0, -1):
+        print(i)
