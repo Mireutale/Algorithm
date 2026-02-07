7b6dd7f0e293211862b69a3c95433dcc93b7395e
mireutale <mireutale@gmail.com>
2026-02-07 21:50:10 +0900

    Feat: Solved[#17070] 파이프 옮기기 1 문제풀이
    - dp를 활용하는 문제
    - 빈칸의 위치를 파악하고, dp를 활용해서 모든 경우의 수를 체크하는 것이 주요

---
+++ Barkingdog/13.시뮬레이션/BOJ_17070/17070_파이프 옮기기 1.py
@@ -0,0 +1,31 @@
+"""
+* CodingTest/Barkingdog/13.시뮬레이션/BOJ_17070/17070_파이프 옮기기 1.py
+* Author : mireutale
+"""
+
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+if __name__ == "__main__":
+    N = int(input())
+    # 0 = 가로, 1 = 대각선, 2 = 세로
+    count = [[[0] * N for _ in range(N)] for _ in range(3)]
+    wall = [[] for _ in range(N)]
+    for i in range(N):
+        wall[i] = list(map(int, input().split()))
+
+    count[0][0][1] = 1
+    for i in range(2, N):
+        if wall[0][i] != 1:
+            count[0][0][i] = count[0][0][i-1]
+    
+    for i in range(1, N):
+        for j in range(1, N):
+            if wall[i][j] == 0 and wall[i-1][j] == 0 and wall[i][j-1] == 0:
+                count[1][i][j] = count[0][i-1][j-1] + count[1][i-1][j-1] + count[2][i-1][j-1]
+            
+            if wall[i][j] == 0:
+                count[0][i][j] = count[0][i][j-1] + count[1][i][j-1]
+                count[2][i][j] = count[1][i-1][j] + count[2][i-1][j]
+    
+    print(sum(count[i][N-1][N-1] for i in range(3)))
