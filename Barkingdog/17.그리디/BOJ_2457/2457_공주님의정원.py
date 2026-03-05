1ea59cde446d84ea94ec3e7f2cb82513f5120095
mireutale <mireutale@gmail.com>
2026-03-06 02:55:05 +0900

    Fix: [BKD] 2457_공주님의 정원 문제 코드 수정
    - 필요없는 로직 삭제
    - 자세한 설명을 위한 주석 추가

--- Barkingdog/17.그리디/BOJ_2457/2457_공주님의정원.py
+++ Barkingdog/17.그리디/BOJ_2457/2457_공주님의정원.py
@@ -1,7 +1,4 @@
-"""
-* Codes\something\BOJ_2457\2457_공주님의정원.py
-* Author : mireutale
-"""
+# Barkingdog/17.그리디/BOJ_2457/2457_공주님의정원.py
 
 import sys
 input = lambda: sys.stdin.readline().rstrip()
@@ -13,8 +10,11 @@ if __name__ == "__main__":
     for _ in range(flowers_number):
         # * 피는 월, 피는 일, 지는 월, 지는 일
         start_month, start_day, last_month, last_day = map(int, input().split())
+        # ? x월 y일인 경우, xy의 형태로 저장 -> 3월 1일 = 301
         flower_list.append([start_month*100 + start_day, last_month*100 + last_day])
     # * 첫번째 원소는 오름차순, 첫번째 원소가 같은 경우, 두번째 원소 기준으로는 내림차순
+    # ? 꽃이 피는 일을 기준으로는 오름차순, 지는 일을 기준으로는 내림차순
+    # ? 똑같은 날에 피고, 더 빨리지는 꽃은 필요가 없음
     flower_list = deque(sorted(flower_list, key=lambda x: (x[0], -x[1])))
     
     start = 301
@@ -22,15 +22,16 @@ if __name__ == "__main__":
     temp_dict = {}
     
     while flower_list:
+        # ? 개화, 낙화
         flowering, wither = flower_list.popleft()
         
         if start <= 1130:
+            # 현재 시작일보다 개화시기가 전이라면
             if flowering <= start:
                 # 같은 시작일이 있으면 더 큰 wither 값으로 업데이트
-                if flowering in temp_dict:
-                    temp_dict[flowering] = max(temp_dict[flowering], wither)
-                else:
+                if flowering not in temp_dict:
                     temp_dict[flowering] = wither
+            # 현재 시작일보다 개화시기가 후 라면
             else:
                 # 현재까지 저장된 꽃들 중 가장 늦게 지는 꽃 선택
                 if temp_dict:
@@ -38,7 +39,7 @@ if __name__ == "__main__":
                     start = max_wither
                     flowers_count += 1
                     temp_dict.clear()
-                    # 현재 꽃도 처리
+                    # 오늘을 기준으로, 가장 늦게 지는 꽃 설정
                     temp_dict[flowering] = wither
     
     # 마지막으로 남은 꽃들 처리
