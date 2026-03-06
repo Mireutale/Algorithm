edbf60715a9676c93f7fee120bb58de727099485
mireutale <mireutale@gmail.com>
2026-03-06 16:33:48 +0900

    Fix: [BKD] 11403_경로 찾기 문제 풀이 수정

--- Barkingdog/24.그래프/BOJ_11403/11403_경로 찾기.py
+++ Barkingdog/24.그래프/BOJ_11403/11403_경로 찾기.py
@@ -1,19 +1,17 @@
-# Barkingdog/17.그리디/BOJ_11399/11399_ATM.py
+# Barkingdog/24.그래프/BOJ_11403/11403_경로 찾기.py
 
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-from collections import deque
 
 if __name__ == "__main__":
     n = int(input())
-    p = map(int, input().split())
-    p = deque(sorted(p))
-
-    last = 0
-    total = 0
-
-    while p:
-        last += p.popleft()
-        total += last
-        
-    print(total)
+    edges = [list(map(int, input().split())) for _ in range(n)]
+    # ? i -> k -> j 형태로 이동가능한 모든 경로를 체크하자.
+    for k in range(n): # 중간 경유지
+        for i in range(n): # 시작 노드
+            for j in range(n): # 도착 노드
+                if edges[i][k] == 1 and edges[k][j] == 1:
+                    edges[i][j] = 1
+    
+    for row in edges:
+        print(*row)
